66572575e2143f4e2bf1bff8b979829f1fd583cf
20160553 <53904156+20160553@users.noreply.github.com>
2021-08-08 15:22:10 +0900

    HomeFragment.kt 구성

--- app/src/main/java/com/kit20160553/aop_part3_chapter06/home/HomeFragment.kt
+++ app/src/main/java/com/kit20160553/aop_part3_chapter06/home/HomeFragment.kt
@@ -1,7 +1,30 @@
 package com.kit20160553.aop_part3_chapter06.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kit20160553.aop_part3_chapter06.R
+import com.kit20160553.aop_part3_chapter06.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    private var binding: FragmentHomeBinding? = null
+    private lateinit var articleAdapter: ArticleAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+        articleAdapter = ArticleAdapter()
+        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
+            add(ArticleModel("0", "aaaa", 10000, "5000원", ""))
+            add(ArticleModel("0", "ㅠㅠㅠㅠ", 210000, "51000원", ""))
+        })
+
+        fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+    }
+
 }
